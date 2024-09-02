6b8eb5a131f6097a971fb33fed71406746a17a48
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-09-02 15:17:21 +0500

    npm added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const { indexRoutes } = require('./routes/index.routes')
-require("./config/mongodb.config")
+require('./config/mongodb.config')
+const { logger, logRequestDuration } = require('./utils/winston')
+
+app.use(logRequestDuration)
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.url}`)
+  next()
+})
 
 app.use('/', indexRoutes)
 
