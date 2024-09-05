156582e7d8d9b6e64ba7e83435fa2a490161aca2
Muhammad Hamza Liaqat <mh408800@gmail.com>
2024-09-05 10:41:31 +0500

    association fix

--- src/config/association.js
+++ src/config/association.js
@@ -10,12 +10,12 @@ const associationDefined = () => {
   Car.belongsTo(User, { foreignKey: 'user_id' })
 
   // user has many orders, Order belongs to user
-  User.hasMany(Order, { foreignKey: user_id })
-  Order.belongsTo(User, { foreignKey: user_id })
+  User.hasMany(Order, { foreignKey: "user_id" })
+  Order.belongsTo(User, { foreignKey: "user_id" })
 
   // car has many orders, order belongs to car
-  Car.hasMany(Order, { foreignKey: car_id })
-  Order.belongsTo(Car, { foreignKey: car_id })
+  Car.hasMany(Order, { foreignKey: "car_id" })
+  Order.belongsTo(Car, { foreignKey: "car_id" })
 }
 
 module.exports = associationDefined
