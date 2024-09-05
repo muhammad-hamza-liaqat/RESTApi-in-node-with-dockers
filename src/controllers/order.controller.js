681538138815c38a62efeb3afec9139cc29adfab
Muhammad Hamza Liaqat <mh408800@gmail.com>
2024-09-05 15:10:12 +0500

    check added in order.controller.js

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,11 +1,27 @@
 const Order = require('../models/order.model')
-const { HTTPResponse } = require('../helpers/response')
+const { HTTPResponse, HTTPError } = require('../helpers/response')
 const { StatusCodes } = require('http-status-codes')
+const Car = require('../models/car.model')
 
 const addOrderForCar = async (req, res) => {
-  let response
+  let response, error
   const { orderName, carId, orderDescription } = req.body
   const user = req.user
+  const car = await Car.findByPk(carId)
+
+  if (!car) {
+    error = new HTTPError('Invalid Car ID', StatusCodes.BAD_REQUEST)
+    return res.status(StatusCodes.BAD_REQUEST).json(error)
+  }
+
+  if (car.user_id !== user.id) {
+    error = new HTTPError(
+      `pass the valid car_id for ${user.userName}`,
+      StatusCodes.CONFLICT
+    )
+    return res.status(StatusCodes.CONFLICT).json(error)
+  }
+
   const newOrder = await Order.create({
     order_name: orderName,
     car_id: carId,
