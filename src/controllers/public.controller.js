b1e97fcbeb006d3a85a3b9aab54aac91d7234673
Muhammad Hamza Liaqat <mh408800@gmail.com>
2024-09-12 14:29:19 +0500

    get apis

--- src/controllers/public.controller.js
+++ src/controllers/public.controller.js
@@ -1,5 +1,53 @@
-const allCustomers = async (req,res)=>{
-    res.end("hello")
+const { StatusCodes } = require('http-status-codes')
+const { HTTPResponse } = require('../helpers/response')
+const Customer = require('../models/customer.model')
+const Category = require('../models/category.model')
+const Order = require('../models/order.model')
+const orderDetail = require('../models/orderDetail.model')
+const Product = require('../models/product.model')
+const testProduct = require('../models/testProduct.model')
+
+const allCustomers = async (req, res) => {
+  const customers = await Customer.findAndCountAll()
+  let response = new HTTPResponse('Fetched successfully', customers)
+  return res.status(StatusCodes.OK).json(response)
 }
 
-module.exports = { allCustomers }
+const allCategories = async (req, res) => {
+  const category = await Category.findAndCountAll()
+  let response = new HTTPResponse('Fetched successfully', category)
+  return res.status(StatusCodes.OK).json(response)
+}
+
+const allOrders = async (req, res) => {
+  const orders = await Order.findAndCountAll()
+  let response = new HTTPResponse('Fetched successfully', orders)
+  return res.status(StatusCodes.OK).json(response)
+}
+
+const allOrderDetails = async (req, res) => {
+  const details = await orderDetail.findAndCountAll()
+  let response = new HTTPResponse('Fetched successfully', details)
+  return res.status(StatusCodes.OK).json(response)
+}
+
+const allProducts = async (req, res) => {
+  const products = await Product.findAndCountAll()
+  let response = new HTTPResponse('Fetched successfully', products)
+  return res.status(StatusCodes.OK).json(response)
+}
+
+const allTestProducts = async (req, res) => {
+  const testProducts = await testProduct.findAndCountAll()
+  let response = new HTTPResponse('Fetched successfully', testProducts)
+  return res.status(StatusCodes.OK).json(response)
+}
+
+module.exports = {
+  allCustomers,
+  allCategories,
+  allOrders,
+  allOrderDetails,
+  allProducts,
+  allTestProducts
+}
--- src/routes/publicRoutes/public.routes.js
+++ src/routes/publicRoutes/public.routes.js
@@ -1,7 +1,20 @@
-const express = require("express");
-const { allCustomers } = require("../../controllers/public.controller");
-const publicRoutes = express.Router();
+const express = require('express')
+const {
+  allCustomers,
+  allOrders,
+  allOrderDetails,
+  allProducts,
+  allCategories,
+  allTestProducts
+} = require('../../controllers/public.controller')
+const publicRoutes = express.Router()
+const { catchAsyncErrors } = require('../../helpers/tryCatch')
 
-publicRoutes.get("/customer", allCustomers)
+publicRoutes.get('/customers', catchAsyncErrors(allCustomers))
+publicRoutes.get('/orders', catchAsyncErrors(allOrders))
+publicRoutes.get('/order-details', catchAsyncErrors(allOrderDetails))
+publicRoutes.get('/products', catchAsyncErrors(allProducts))
+publicRoutes.get('/category', catchAsyncErrors(allCategories))
+publicRoutes.get('/test-product', catchAsyncErrors(allTestProducts))
 
 module.exports = { publicRoutes }
